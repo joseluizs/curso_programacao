d627965f508d6a563a5c185c9ebb5bea5e4e2f82
joseluizs <joseluiz.smarques@gmail.com>
2022-09-19 14:52:25 -0300

    Herança

---
+++ src/application/Aula157.java
@@ -0,0 +1,14 @@
+package application;
+
+import entities.BusinesssAccount;
+
+public class Aula157 {
+
+	public static void main(String[] args) {
+		
+		BusinesssAccount account = new BusinesssAccount();
+		account.deposit(10);
+
+	}
+
+}
