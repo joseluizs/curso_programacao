d25136df0fe36677cd078a15b32a785d2a2532f8
joseluizs <joseluiz.smarques@gmail.com>
2022-09-23 19:41:30 -0300

    Update USATaxService.java

--- src/model/services/USATaxService.java
+++ src/model/services/USATaxService.java
@@ -3,7 +3,7 @@ package model.services;
 public class USATaxService implements TaxService {
 	
 	public double tax(double amount) {
-		if (amount <= 200.0) {
+		if (amount <= 100.0) {
 			return amount * 0.4;
 		} else {
 			return amount * 0.30;
