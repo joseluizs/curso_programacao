34a591d4971baa21b2d92d44d1530d941db9a000
joseluizs <joseluiz.smarques@gmail.com>
2022-10-03 19:44:41 -0300

    Tipo Coringa

---
+++ src/application/Aula239TiposCoringa.java
@@ -0,0 +1,15 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Aula239TiposCoringa {
+
+	public static void main(String[] args) {
+		List<?> myObjs = new ArrayList<Object>();
+		List<Integer> myNumbers = new ArrayList<Integer>();
+		myObjs = myNumbers;
+
+	}
+
+}
