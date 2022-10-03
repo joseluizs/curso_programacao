e798082889993ec3502575db5dfb0986887c2d32
joseluizs <joseluiz.smarques@gmail.com>
2022-10-03 20:04:16 -0300

    Curinga
    Princípio get/put - covariância

--- src/application/Aula239TiposCoringa.java
+++ src/application/Aula239TiposCoringa.java
@@ -1,15 +1,20 @@
 package application;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Aula239TiposCoringa {
-
 	public static void main(String[] args) {
-		List<?> myObjs = new ArrayList<Object>();
-		List<Integer> myNumbers = new ArrayList<Integer>();
-		myObjs = myNumbers;
-
+		List<Integer> myInts = Arrays.asList(5, 2, 10);
+		printList(myInts);
+		
+		List<String> myStrs = Arrays.asList("MARIA", "Alex", "Bob");
+		printList(myStrs);
 	}
 
+	public static void printList(List<?> list) {
+		for (Object obj : list) {
+			System.out.println(obj);
+		}
+	}
 }
