fcda2236cee117d69e647ddd6e038a84836de552
joseluizs <joseluiz.smarques@gmail.com>
2022-10-03 20:19:03 -0300

    Curinga Delimitados

--- src/application/Aula240CuringasDelimitados.java
+++ src/application/Aula240CuringasDelimitados.java
@@ -1,22 +1,48 @@
 package application;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Aula240CuringasDelimitados {
 
 	public static void main(String[] args) {
-		List<Integer> intList = new ArrayList<Integer>();
-		intList.add(10);
-		intList.add(5);
-		List<? extends Number> list = intList;
-		
-		Number x = list.get(0);
-		
-		// list.add(20); // erro de compilacao
-		// get OK
-		// put Error
+		/*
+		 * List<Integer> intList = new ArrayList<Integer>(); intList.add(10);
+		 * intList.add(5); List<? extends Number> list = intList;
+		 * 
+		 * Number x = list.get(0);
+		 * 
+		 * // list.add(20); // erro de compilacao // get OK // put Error
+		 * 
+		 * List<Object> myObjs = new ArrayList<Object>(); myObjs.add("Maria");
+		 * myObjs.add("Alex"); List<? super Number> myNums = myObjs;
+		 * 
+		 * myNums.add(10); myNums.add(3.14);
+		 * 
+		 * //Contravariância // Number x = myNums.get(0); // erro de compilacao // get
+		 * Error // put ok
+		 */
+		List<Integer> myInts = Arrays.asList(1, 2, 3, 4);
+		List<Double> myDoubles = Arrays.asList(3.14, 6.28);
+		List<Object> myObjs = new ArrayList<Object>();
+		copy(myInts, myObjs);
+		printList(myObjs);
+		copy(myDoubles, myObjs);
+		printList(myObjs);
+	}
 
+	public static void copy(List<? extends Number> source, List<? super Number> destiny) {
+		for (Number number : source) {
+			destiny.add(number);
+		}
+	}
+
+	public static void printList(List<?> list) {
+		for (Object obj : list) {
+			System.out.print(obj + " ");
+		}
+		System.out.println();
 	}
 
 }
