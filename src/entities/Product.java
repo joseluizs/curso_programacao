ebb217d55dbfed8ddc804d8503b21b114d16b28e
joseluizs <joseluiz.smarques@gmail.com>
2022-10-04 08:07:38 -0300

    TreeSet
    Recordando as implementações
    • Principais implementações:
    • HashSet - mais rápido (operações O(1) em tabela hash) e não ordenado
    • TreeSet - mais lento (operações O(log(n)) em árvore rubro-negra) e ordenado
    pelo compareTo do objeto (ou Comparator)
    • LinkedHashSet - velocidade intermediária e elementos na ordem em que são
    adicionados

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -2,7 +2,7 @@ package entities;
 
 import java.util.Objects;
 
-public class Product {
+public class Product implements Comparable<Product>{
 
 	private String name;
 	private Double price;
@@ -49,6 +49,16 @@ public class Product {
 			return false;
 		Product other = (Product) obj;
 		return Objects.equals(name, other.name) && Objects.equals(price, other.price);
+	}
+
+	@Override
+	public String toString() {
+		return "Product [name=" + name + ", price=" + price + "]";
+	}
+
+	@Override
+	public int compareTo(Product other) {
+		return name.toUpperCase().compareTo(other.getName().toUpperCase());
 	}
 	
 	
