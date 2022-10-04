f0c6819b4037a1a29b86e51b8f186eb074f9e787
joseluizs <joseluiz.smarques@gmail.com>
2022-10-04 07:41:43 -0300

    Set Igualdade
    Como as coleções Hash testam igualdade?
    • Se hashCode e equals estiverem implementados:
    • Primeiro hashCode. Se der igual, usa equals para confirmar.
    • Lembre-se: String, Integer, Double, etc. já possuem equals e hashCode
    • Se hashCode e equals NÃO estiverem implementados:
    • Compara as referências (ponteiros) dos objetos.

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -1,5 +1,7 @@
 package entities;
 
+import java.util.Objects;
+
 public class Product {
 
 	private String name;
@@ -31,4 +33,23 @@ public class Product {
 				+ " $ " 
 				+ String.format("%.2f", price);
 	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name, price);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return Objects.equals(name, other.name) && Objects.equals(price, other.price);
+	}
+	
+	
 }
