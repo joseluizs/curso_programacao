8500a47df87eb813b140d517e6067490842816af
joseluizs <joseluiz.smarques@gmail.com>
2022-10-08 11:49:41 -0300

    Implementação da interface

---
+++ src/application/Aula256Consumer.java
@@ -0,0 +1,35 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import entities.Product;
+import util.PriceUpdate;
+
+			/**
+			 * 
+			 * @author Familia LCH
+			 * Fazer um programa que, a partir de uma lista de produtos, aumente o preço dos produtos em 10%.
+			 *
+			 */
+
+public class Aula256Consumer {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		List<Product> list = new ArrayList<>();
+
+		list.add(new Product("Tv", 900.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("Tablet", 350.50));
+		list.add(new Product("HD Case", 80.90));
+		
+		list.forEach(new PriceUpdate());
+		
+		list.forEach(System.out::println);//reference method para println
+
+	}
+
+}
