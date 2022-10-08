827c3c24f768780eaa2c448ad05d16d3d4b6658e
joseluizs <joseluiz.smarques@gmail.com>
2022-10-08 15:25:44 -0300

    Stream

--- src/application/Aula259Stream.java
+++ src/application/Aula259Stream.java
@@ -9,7 +9,7 @@ public class Aula259Stream {
 	public static void main(String[] args) {
 		List<Integer> list = Arrays.asList(3, 4, 5, 10, 7);
 		
-		Stream<Integer> st1 = list.stream();
+		Stream<Integer> st1 = list.stream().map(x -> x * 10);
 		System.out.println(Arrays.toString(st1.toArray()));
 		
 		Stream<String> st2 = Stream.of("Maria", "Alex", "Bob");
