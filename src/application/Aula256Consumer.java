de064106ef6f60a27c15a9e42da28ee75aa51219
joseluizs <joseluiz.smarques@gmail.com>
2022-10-08 12:03:17 -0300

    • Expressão lambda declarada

--- src/application/Aula256Consumer.java
+++ src/application/Aula256Consumer.java
@@ -3,6 +3,7 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Consumer;
 
 import entities.Product;
 import util.PriceUpdate;
@@ -26,7 +27,13 @@ public class Aula256Consumer {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		list.forEach(Product::nonStaticPriceUpdate);
+		double factor = 1.1;
+		
+		Consumer<Product> cons = p ->{ // como tem apenas uma linha as chaves são opcionais!
+			p.setPrice(p.getPrice() * factor);
+		};
+		
+		list.forEach(cons);
 		
 		list.forEach(System.out::println);//reference method para println
 
